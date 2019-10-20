d5288d61fb48c6d5aa1c33804608208297fec7df
GambuzX <mg_mesquita@hotmail.com>
2019-10-20 19:33:08 +0100

    Knight movement and attacks

--- Assets/Scripts/knight_movement.cs
+++ Assets/Scripts/knight_movement.cs
@@ -5,20 +5,81 @@ using UnityEngine;
 public class knight_movement : MonoBehaviour
 {
     public float speed;
+    public float jumpForce;
+
+    public float jumpCooldown = 1f;
+    public float attackCooldown = 1f;
+
     private Rigidbody2D rb;
+    private Animator animator;
+
+    private bool grounded;
+    private bool jumpLock;
+    private bool attackLock;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
+        grounded = true;
+        jumpLock = true;
+        attackLock = false;
+    }
+
+    void Update() {
+        
+        if (!attackLock && Input.GetKeyDown(KeyCode.X)) {
+            animator.SetTrigger("attack_ranged");
+            attackLock = true;
+            Invoke("unlockAttack", attackCooldown);
+        }
+        
+        if (!attackLock && Input.GetKeyDown(KeyCode.Y)) {
+            animator.SetTrigger("attack_close_1");
+            attackLock = true;
+            Invoke("unlockAttack", attackCooldown);
+        }
+        
+        if (!attackLock && Input.GetKeyDown(KeyCode.Z)) {
+            animator.SetTrigger("attack_close_2");
+            attackLock = true;
+            Invoke("unlockAttack", attackCooldown);
+        }
+
     }
 
     void FixedUpdate()
     {
+
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+        Vector3 horiz_mov = new Vector3(moveHorizontal, 0.0f, 0.0f);
+        animator.SetFloat("horiz_mov", moveHorizontal);
 
-        rb.AddForce(movement * speed);        
+        if (moveHorizontal > 0) {
+            transform.eulerAngles = new Vector3(0.0f, 0, 0.0f);
+        }
+        else if (moveHorizontal < 0) {
+            transform.eulerAngles = new Vector3(0.0f, 180, 0.0f);
+        }
+   
+        if (!attackLock)
+            rb.velocity = horiz_mov * speed;
+
+        if (grounded && moveVertical > 0) {
+            animator.SetTrigger("jump");
+            rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Force);
+            grounded = false;
+            Invoke("unlockJump", jumpCooldown);
+        }
+    }
+
+    void unlockJump() {
+        grounded = true;
+    }
+
+    void unlockAttack() {
+        attackLock = false;
     }
 }
