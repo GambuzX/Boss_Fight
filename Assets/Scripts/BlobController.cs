335324f365600f4cbbd559383338c06ace428b9c
GambuzX <mg_mesquita@hotmail.com>
2019-10-20 21:59:04 +0100

    Blob attacks; Health Bar

---
+++ Assets/Scripts/BlobController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobController : MonoBehaviour
+{
+
+    public float attackCooldown = 2f;
+
+    private bool attackLock;
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        attackLock = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (!attackLock) {
+
+            int choice = Random.Range(1, 20);
+            if (choice <= 4)
+                triggerAttack("swipe");
+            else if (choice >= 6 && choice <= 12)
+                triggerAttack("punch");
+            else if (choice >= 15 && choice <= 16)
+                triggerAttack("balls");
+            else {
+                triggerAttack("none");
+            }
+        }
+
+    }
+
+    void triggerAttack(string attack) {
+        float cooldown = attackCooldown;
+        if (attack == "none") {
+            cooldown = Random.Range(0.1f, 0.2f);
+        }
+        else 
+            animator.SetTrigger(attack);
+        attackLock = true;
+        Invoke("unlockAttack", attackCooldown);
+    }
+
+    void unlockAttack() {
+        attackLock = false;
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    private int health = 4;
+    private RectTransform healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 4;    
+        healthBar = GameObject.Find("Health_Bar").GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void loseHealth() {
+        if (health <= 0) return;
+        health--;
+        updateHealthBar();
+    }
+
+    private void updateHealthBar() {
+        healthBar.localScale = new Vector3(health * 0.25f, healthBar.localScale.y, 1);
+    }
+}
